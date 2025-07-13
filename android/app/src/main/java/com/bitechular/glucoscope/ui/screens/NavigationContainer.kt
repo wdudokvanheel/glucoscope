89851fe5731d46b117f61e78370b841239c3964d
wdudokvanheel <wesley@bitechular.com>
2025-07-13 21:58:01 +0200

    Move scaffold inside MainScreen

--- android/app/src/main/java/com/bitechular/glucoscope/ui/screens/NavigationContainer.kt
+++ android/app/src/main/java/com/bitechular/glucoscope/ui/screens/NavigationContainer.kt
@@ -1,19 +1,12 @@
 package com.bitechular.glucoscope.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.bitechular.glucoscope.preference.PreferenceModel
 import com.bitechular.glucoscope.ui.screens.configuration.ConfigurationScreen
 import com.bitechular.glucoscope.ui.screens.main.MainScreen
 
@@ -31,26 +24,12 @@ fun NavigationContainer() {
     val navController = rememberNavController()
 
     CompositionLocalProvider(Navigator provides navController) {
-        val prefs = PreferenceModel.current
-
-        Scaffold(
-            modifier = Modifier
-                .fillMaxSize()
-        ) { innerPadding ->
-            Column(
-                Modifier
-                    .background(prefs.theme.background)
-                    .padding(innerPadding)
-                    .fillMaxSize()
-            ) {
-                NavHost(
-                    navController = navController,
-                    startDestination = Screen.Main.route
-                ) {
-                    composable(Screen.Main.route) { MainScreen() }
-                    composable(Screen.Config.route) { ConfigurationScreen() }
-                }
-            }
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Main.route
+        ) {
+            composable(Screen.Main.route) { MainScreen() }
+            composable(Screen.Config.route) { ConfigurationScreen() }
         }
     }
 }
--- android/app/src/main/java/com/bitechular/glucoscope/ui/screens/main/MainScreen.kt
+++ android/app/src/main/java/com/bitechular/glucoscope/ui/screens/main/MainScreen.kt
@@ -1,18 +1,24 @@
 package com.bitechular.glucoscope.ui.screens.main
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import com.bitechular.glucoscope.data.model.GlucoseMeasurement
 import com.bitechular.glucoscope.data.repository.DemoDataSource
+import com.bitechular.glucoscope.preference.PreferenceModel
 import com.bitechular.glucoscope.ui.components.graph.ThemedGraph
 import com.bitechular.glucoscope.ui.components.indicator.Indicator
 
 @Composable
 fun MainScreen() {
+    val prefs = PreferenceModel.current
+
     // TODO Move this to RealTimeDataService
     val dataSource = DemoDataSource()
 
@@ -24,9 +30,19 @@ fun MainScreen() {
         value = dataSource.getCurrentValue()
     }
 
-    Column {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            Modifier
+                .background(prefs.theme.background)
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
 //                ReactivePrefTest()
-        Indicator(currentValue)
-        ThemedGraph(measurements, Modifier.fillMaxSize())
+            Indicator(currentValue)
+            ThemedGraph(measurements, Modifier.fillMaxSize())
+        }
     }
 }
