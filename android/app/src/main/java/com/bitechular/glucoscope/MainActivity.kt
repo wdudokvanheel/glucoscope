42c6cbb418f86fb9c7960530c221c08a37e9a145
wdudokvanheel <wesley@bitechular.com>
2025-07-17 14:39:17 +0200

    Change Android's status bar color based on the current theme

--- android/app/src/main/java/com/bitechular/glucoscope/MainActivity.kt
+++ android/app/src/main/java/com/bitechular/glucoscope/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.bitechular.glucoscope
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.bitechular.glucoscope.extensions.isLight
 import com.bitechular.glucoscope.preference.GlucoScopePreferences
 import com.bitechular.glucoscope.preference.PreferenceModel
 import com.bitechular.glucoscope.ui.screens.AppNavigationView
@@ -18,11 +24,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val preferences: GlucoScopePreferences = hiltViewModel()
+            val prefs: GlucoScopePreferences = hiltViewModel()
 
-            CompositionLocalProvider(PreferenceModel provides preferences) {
+            StatusBarUpdater(prefs)
+
+            CompositionLocalProvider(PreferenceModel provides prefs) {
                 AppNavigationView()
             }
+        }
+    }
+
+    @Composable
+    fun StatusBarUpdater(prefs: GlucoScopePreferences) {
+        val ctx = LocalContext.current
+        LaunchedEffect(prefs.theme.background, prefs.theme.isLight) {
+            val window = (ctx as Activity).window
+            WindowCompat.getInsetsController(
+                window,
+                window.decorView
+            ).isAppearanceLightStatusBars = prefs.theme.isLight
         }
     }
 }
